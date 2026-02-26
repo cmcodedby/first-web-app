422c0a04b50dc8d2f6a97a12dde70a8d798d68b8
cmcodedby <cartier.medina@codedbykids.org>
2026-02-26 17:26:23 -0500

    Added console commands describing variables

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 console.log("hello world!")
 console.log("There are a lot of opportunities")
+const name = "cartier";
+let age = 18;
+let islearningjs = true;
+console.log(name + " " + typeof name)
+console.log(age + " " + typeof age)
+console.log(islearningjs + " "+ typeof islearningjs)
+
+console.log(`My name is ${name}. The type is ${typeof name}`)
+console.log(`My age is ${age}. The type is ${typeof age}`)
